fec6dc81acf9b145b1195413d22d48a571f9f47e
bulutluoz <abbulutluoz@gmail.com>
2024-09-17 12:14:04 +0200

    day02 sonu

--- src/day02_scanner/C06_KullaniciDegerleriniYazdirma.java
+++ src/day02_scanner/C06_KullaniciDegerleriniYazdirma.java
@@ -1,13 +1,41 @@
 package day02_scanner;
 
+import java.util.Scanner;
+
 public class C06_KullaniciDegerleriniYazdirma {
 
     public static void main(String[] args) {
 
-        //Soru 3- Kullanicidan ismini, soyismini ve yasini alip, asagidaki formatta yazdirin.
+        //Soru 3- Kullanicidan ismini, soyismini ve yasini alip,
+        //        asagidaki formatta yazdirin.
         //		Isminiz : John
         //		Soyisminiz : Doe
         //		Yasiniz : 44
         //		Kaydiniz basariyla tamamlanmistir.
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Lutfen isminizi giriniz...");
+        // String girilenIsim1 = scanner.next(); // bu ilk space'e kadar olan kismi alir
+        String girilenIsim = scanner.nextLine(); // butun satiri alir
+
+
+        System.out.println("Lutfen soyisminizi giriniz...");
+        String girilenSoyisim = scanner.nextLine();
+
+        System.out.println("Lutfen yasinizi giriniz...");
+        int yas = scanner.nextInt();
+
+        System.out.println(
+             "Isminiz : " + girilenIsim +
+             "\nSoyisminiz : " + girilenSoyisim +
+             "\nYasiniz : " + yas +
+             "\nKaydiniz basariyla tamamlanmistir."
+        );
+
+
+
+
     }
 }
