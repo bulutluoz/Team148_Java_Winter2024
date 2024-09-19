0da09e3c79690fb788ce9197406dd91488e4ba20
Murat BABAYIGIT <babayigit@gmail.com>
2024-09-19 13:42:23 +0300

    babayigit ilk ders

---
+++ src/day04_matIslemler_Concatenation_Logical/C02_matematikselIslemler.java
@@ -0,0 +1,38 @@
+package day04_matIslemler_Concatenation_Logical;
+
+import java.util.Scanner;
+
+public class C02_matematikselIslemler {
+
+    public static void main(String[] args) {
+        // Kullanicidan 4 basamakli pozitif bir tamsayi alip rakamlar toplamini bulalim
+
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Lütfen 4 basamaklı bir sayı giriniz: ");
+        int sayi= scan.nextInt();
+        System.out.println("Girmiş olduğunuz sayı: "+sayi); //4586
+
+        int birlerBasamagi=sayi%10; // 6
+        System.out.println("birlerBasamagi = " + birlerBasamagi);
+        sayi=sayi/10;  //sayi: 458
+
+        int onlarbasamagi=sayi%10; // 8
+        System.out.println("onlarbasamagi = " + onlarbasamagi);
+        sayi=sayi/10; //45
+
+        int yuzlerBasamagi=sayi%10;  // 5
+        System.out.println("yuzlerBasamagi = " + yuzlerBasamagi);
+
+        int binlerBasamagi=sayi/10;
+        System.out.println("binlerBasamagi = " + binlerBasamagi);
+
+      //  int rakamlarToplami=binlerBasamagi+yuzlerBasamagi+onlarbasamagi+birlerBasamagi;
+      //  System.out.println("Girilen Sayının Rakamları Toplamı = " + rakamlarToplami);
+
+        System.out.println("Girilen Sayının Rakamları Toplamı = " + (binlerBasamagi+yuzlerBasamagi+onlarbasamagi+birlerBasamagi));
+
+
+
+
+    }
+}
---
+++ src/day04_matIslemler_Concatenation_Logical/C03_Concatenation.java
@@ -0,0 +1,29 @@
+package day04_matIslemler_Concatenation_Logical;
+
+public class C03_Concatenation {
+    public static void main(String[] args) {
+
+        String str1="Java";
+        String str2="Candır";
+        String str3=" ";
+        String str4="";
+
+        int a=2;
+        int b=3;
+
+        System.out.println(str1+str3+str2+str3+str4+a+b); // Java Candır 23
+
+        System.out.println(str1+str3+str2+str3+str4+(a+b)); // Java Candır 5
+
+        //Bir kaç variable System.out.println ile yazdırılmak istenilince concatenaton yapılır
+        // Bu işlem yapılırken matematiksel işlem önceliğine dikkat etmek şartıyla
+        // String bir değer var ise string diğer değerleri string'e çevirerek concat eder.
+
+        System.out.println(a+b+str3+str1+str3+str2+str3); // 5 Java Candır
+
+        System.out.println(a*b+str2+str4); // 6Candır
+
+
+
+    }
+}
