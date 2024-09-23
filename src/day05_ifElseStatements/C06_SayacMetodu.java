296074776353b1921a5cb961d114cc7d0620cc26
bulutluoz <abbulutluoz@gmail.com>
2024-09-23 10:35:48 +0200

    Day05 sonu

--- src/day05_ifElseStatements/C06_SayacMetodu.java
+++ src/day05_ifElseStatements/C06_SayacMetodu.java
@@ -2,7 +2,7 @@ package day05_ifElseStatements;
 
 import java.util.Scanner;
 
-public class C05_BagimsizIfleriTopluDegerlendirme {
+public class C06_SayacMetodu {
 
     public static void main(String[] args) {
 
@@ -21,25 +21,60 @@ public class C05_BagimsizIfleriTopluDegerlendirme {
         System.out.println("Lutfen iki tamsayi giriniz...");
         int sayi1 = scanner.nextInt();
         int sayi2 = scanner.nextInt();
+
+        int sayac = 0;
+
         // 1- sayi1 < sayi2 ise "birinci sayi daha kucuk"
-        if (sayi1<sayi2) System.out.println("birinci sayi daha kucuk");
+        if (sayi1<sayi2) {
+            System.out.println("birinci sayi daha kucuk");
+            sayac++;
+        }
 
         // 2- sayi1 pozitif ise "birinci sayi sifirdan buyuk"
-        if (sayi1 > 0) System.out.println("birinci sayi sifirdan buyuk");
+        if (sayi1 > 0){
+            System.out.println("birinci sayi sifirdan buyuk");
+            sayac++;
+        }
 
         // 3- sayi2 50'den buyuk ise "ikinci sayi 50'den buyuk"
-        if (sayi2>50) System.out.println("ikinci sayi 50'den buyuk");
+        if (sayi2>50){
+            System.out.println("ikinci sayi 50'den buyuk");
+            sayac++;
+        }
 
         // 4- sayi2 5 ile bolunebiliyorsa "ikinci sayi 5'in tam kati"
-        if (sayi2 % 5 == 0) System.out.println("ikinci sayi 5'in tam kati");
+        if (sayi2 % 5 == 0) {
+            System.out.println("ikinci sayi 5'in tam kati");
+            sayac++;
+        }
 
         // 5- EGER konsolda yukardakilerden hicbiri yazdirilmazsa
         //    "Nasil sayi sectiniz , hicbir sarti saglamiyor" yazdirin
 
-        if (  !(sayi1<sayi2) && !(sayi1 > 0) && !(sayi2>50) &&  !(sayi2 % 5 == 0)  ){
-            System.out.println("Nasil sayi sectiniz , hicbir sarti saglamiyor");
-        }
+        // buraya geldiginde sayac 0, 1, 2, 3, 4 olabilir
+
+        if (sayac == 0 ) System.out.println("Nasil sayi sectiniz , hicbir sarti saglamiyor");
+
+
+        // 6- Eger tum if body'leri calistiysa
+        //    "mukemmel sayilar secmissiniz" yazdirin
+
+        if (sayac == 4) System.out.println("mukemmel sayilar secmissiniz");
+
+        // 7 - eger 2 sarti saglamissa
+        //     "idare eder" yazdirin
+
+        if (sayac == 2) System.out.println("idare eder...");
+
+
+
 
 
     }
+
+
+
+
+
+
 }
--- src/day05_ifElseStatements/C07_FlagMetodu.java
+++ src/day05_ifElseStatements/C07_FlagMetodu.java
@@ -2,7 +2,7 @@ package day05_ifElseStatements;
 
 import java.util.Scanner;
 
-public class C05_BagimsizIfleriTopluDegerlendirme {
+public class C07_FlagMetodu {
 
     public static void main(String[] args) {
 
@@ -21,25 +21,42 @@ public class C05_BagimsizIfleriTopluDegerlendirme {
         System.out.println("Lutfen iki tamsayi giriniz...");
         int sayi1 = scanner.nextInt();
         int sayi2 = scanner.nextInt();
+
+        int flag = 47;
+
         // 1- sayi1 < sayi2 ise "birinci sayi daha kucuk"
-        if (sayi1<sayi2) System.out.println("birinci sayi daha kucuk");
+        if (sayi1<sayi2) {
+            System.out.println("birinci sayi daha kucuk");
+            flag = 72;
+        }
 
         // 2- sayi1 pozitif ise "birinci sayi sifirdan buyuk"
-        if (sayi1 > 0) System.out.println("birinci sayi sifirdan buyuk");
+        if (sayi1 > 0) {
+            System.out.println("birinci sayi sifirdan buyuk");
+            flag = 72;
+        }
 
         // 3- sayi2 50'den buyuk ise "ikinci sayi 50'den buyuk"
-        if (sayi2>50) System.out.println("ikinci sayi 50'den buyuk");
+        if (sayi2>50) {
+            System.out.println("ikinci sayi 50'den buyuk");
+            flag = 72;
+        }
 
         // 4- sayi2 5 ile bolunebiliyorsa "ikinci sayi 5'in tam kati"
-        if (sayi2 % 5 == 0) System.out.println("ikinci sayi 5'in tam kati");
+        if (sayi2 % 5 == 0) {
+            System.out.println("ikinci sayi 5'in tam kati");
+            flag = 72;
+        }
 
         // 5- EGER konsolda yukardakilerden hicbiri yazdirilmazsa
         //    "Nasil sayi sectiniz , hicbir sarti saglamiyor" yazdirin
 
-        if (  !(sayi1<sayi2) && !(sayi1 > 0) && !(sayi2>50) &&  !(sayi2 % 5 == 0)  ){
-            System.out.println("Nasil sayi sectiniz , hicbir sarti saglamiyor");
-        }
+        // sona gelindiginde flag icin 2 ihtimal var
+        // flag basta verdigimiz degere esitse (47) ==> hic bir sart saglanmamis
+        // flag if bod'lerinde verilen degere esitse (72) ==> en az bir if body calismistir
+        //                                                    ama kac tanesinin calistigini bilmiyoruz
 
+        if (flag == 47) System.out.println("Nasil sayi sectiniz , hicbir sarti saglamiyor");
 
     }
 }
