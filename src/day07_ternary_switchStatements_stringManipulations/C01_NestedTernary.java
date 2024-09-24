d1e07f056ff9a89d8ae6c09287bd8101885cd99f
bulutluoz <abbulutluoz@gmail.com>
2024-09-24 12:49:07 +0200

    day06 sonu

---
+++ src/day07_ternary_switchStatements_stringManipulations/C01_NestedTernary.java
@@ -0,0 +1,15 @@
+package day07_ternary_switchStatements_stringManipulations;
+
+public class C01_NestedTernary {
+
+    public static void main(String[] args) {
+
+        // kullanicidan bir sayi isteyin
+        // 0'dan buyukse pozitif,
+        // 0'dan kucukse negatif,
+        // 0 ise notr yazdirin
+
+
+
+    }
+}
