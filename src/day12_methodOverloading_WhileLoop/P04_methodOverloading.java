ff7448ee36fe2fedc88aeafdb77eeecfbd4577c1
Murat BABAYIGIT <babayigit@gmail.com>
2024-10-03 11:39:56 +0300

    day12.babayigit

--- src/day12_methodOverloading_WhileLoop/P04_methodOverloading.java
+++ src/day12_methodOverloading_WhileLoop/P04_methodOverloading.java
@@ -1,12 +1,27 @@
 package day12_methodOverloading_WhileLoop;
 
+import java.util.Scanner;
+
 public class P04_methodOverloading {
     public static void main(String[] args) {
    /*
 
-        While loop kullanarak kullanicidan alinan sayinin rakamlar toplamini bulun.
+    While loop kullanarak kullanicidan alinan sayinin rakamlar toplamini bulun.
 
 
     */
+
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Lütfen bir sayı giriniz: ");
+        int sayi=scan.nextInt();
+        int rakamlarToplami=0;
+
+        while(sayi>0){
+            rakamlarToplami+=sayi%10;
+            sayi=sayi/10;
+        }
+
+        System.out.println("Rakamlar Toplamı = " + rakamlarToplami);
+
     }
 }
