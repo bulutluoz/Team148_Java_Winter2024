eb1f9d14c0860549a0b55a27eef6bfefb78293b7
bulutluoz <abbulutluoz@gmail.com>
2024-10-15 10:46:48 +0200

    18.gun sonu

--- src/day18_arrayLists_forEachLoop/C01_FibonacciSayilariniYazdir.java
+++ src/day18_arrayLists_forEachLoop/C01_FibonacciSayilariniYazdir.java
@@ -1,10 +1,20 @@
 package day18_arrayLists_forEachLoop;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class C01_FibonacciSayilariniYazdir {
 
     public static void main(String[] args) {
+
+
+        System.out.println(ilkNFibonacciListesi(5)); // [0, 1, 1, 2, 3]
+
+        System.out.println(ilkNFibonacciListesi(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+        System.out.println(ilkNFibonacciListesi(20)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181]
+
 
         // kullanicidan pozitif bir n tamsayisini alip,
         // ilk n tane Fibonacci sayisini ir liste olarak yazdirin
@@ -15,12 +25,31 @@ public class C01_FibonacciSayilariniYazdir {
         System.out.println("Lutfen kac adet fibonacci sayisini yazdirmak istediginizi giriniz...");
         int n = scanner.nextInt();
 
+        List<Integer> fibonacciSerisi = new ArrayList<>();
+
         // kullanici negatif veya 0 girerse ? ==> hata
         // kullanici 1 girerse ? ==> 0
         // kullanici 2 girerse ? ==> 0, 1
         // kullanici 2'den fazla deger girerse ==> 0, 1 .........
 
+        if (n<=0){
+            System.out.println("Girilen deger pozitif olmalidir");
+        } else if (n == 1) {
+            fibonacciSerisi.add(0);
+        } else if (n == 2) {
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+        }else {
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+            // 0 1 1 2 3 5 8 13 21 34 55 89 144 233
 
+            for (int i = 2; i <n ; i++) {
+                fibonacciSerisi.add( fibonacciSerisi.get(i-2) + fibonacciSerisi.get(i-1)  );
+            }
+
+            System.out.println(fibonacciSerisi);
+        }
 
 
     }
@@ -29,6 +58,30 @@ public class C01_FibonacciSayilariniYazdir {
     // Verilen pozitif bir n tamsayisini alarak,
     // bize ilk n tane Fibonacci sayisini bir list olarak donduren bir method olusturun.
 
+    public static List<Integer> ilkNFibonacciListesi(int n) {
+
+        List<Integer> fibonacciSerisi = new ArrayList<>();
+
+        if (n <= 0) {
+            System.out.println("Girilen deger pozitif olmalidir");
+        } else if (n == 1) {
+            fibonacciSerisi.add(0);
+        } else if (n == 2) {
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+        } else {
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+            // 0 1 1 2 3 5 8 13 21 34 55 89 144 233
+
+            for (int i = 2; i < n; i++) {
+                fibonacciSerisi.add(fibonacciSerisi.get(i - 2) + fibonacciSerisi.get(i - 1));
+            }
 
 
+        }
+
+        return fibonacciSerisi;
+
+    }
 }
---
+++ src/day18_arrayLists_forEachLoop/C02_FibonacciSayilariniYazdir.java
@@ -0,0 +1,71 @@
+package day18_arrayLists_forEachLoop;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class C02_FibonacciSayilariniYazdir {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan pozitif bir tamsayi alip,
+        // o tamsayidan kucuk Fibonacci sayilarini bir liste olarak yazdirin.
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen fibonacci sayilarini yazdirmak icin ust sayi sinirini girin...");
+        int maxSayi = scanner.nextInt();
+
+        List<Integer> fibonacciSerisi = new ArrayList<>();
+
+        // negatif deger girilirse ==> hata
+        // 0 girilirse ==> 0
+        // 1 girilirse ==> 0, 1, 1
+        // 1'den buyuk bir deger girilirse ==> 0, 1, 1 ......... maxSayi
+        // 0 1 1 2 3 5 8 13 21 34 55 89 144 233
+
+        if (maxSayi < 0){
+            System.out.println("girilen deger negatif olamaz");
+
+        } else if (maxSayi == 0) {
+            fibonacciSerisi.add(0);
+        } else if (maxSayi == 1) {
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+            fibonacciSerisi.add(1);
+        } else{
+
+            fibonacciSerisi.add(0);
+            fibonacciSerisi.add(1);
+            fibonacciSerisi.add(1);
+
+            int sonrakiSayi = 0;
+            int sonrakiIndex = 3;
+
+            while (sonrakiSayi <= maxSayi ){
+
+                sonrakiSayi = fibonacciSerisi.get(sonrakiIndex-2)+
+                              fibonacciSerisi.get(sonrakiIndex-1);
+
+                if (sonrakiSayi <= maxSayi){
+                    fibonacciSerisi.add(sonrakiSayi);
+                }
+                sonrakiIndex++;
+
+            }
+
+
+            System.out.println(fibonacciSerisi);
+        }
+
+
+    }
+
+    // verilen pozitif bir tamsayiyi alip,
+    // o tamsayidan kucuk Fibonacci sayilarini
+    // bir liste olarak bize donduren bir method olusturun.
+
+
+
+
+
+}
