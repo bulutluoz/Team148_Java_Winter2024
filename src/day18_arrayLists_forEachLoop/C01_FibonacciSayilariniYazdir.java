9f2fffbbb7205a034f745e8c6407af67c0ad7131
bulutluoz <abbulutluoz@gmail.com>
2024-10-15 09:49:03 +0200

    18.gun sonu

---
+++ src/day18_arrayLists_forEachLoop/C01_FibonacciSayilariniYazdir.java
@@ -0,0 +1,34 @@
+package day18_arrayLists_forEachLoop;
+
+import java.util.Scanner;
+
+public class C01_FibonacciSayilariniYazdir {
+
+    public static void main(String[] args) {
+
+        // kullanicidan pozitif bir n tamsayisini alip,
+        // ilk n tane Fibonacci sayisini ir liste olarak yazdirin
+
+        // 0 1 1 2 3 5 8 13 21 34 55 89 144 233
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lutfen kac adet fibonacci sayisini yazdirmak istediginizi giriniz...");
+        int n = scanner.nextInt();
+
+        // kullanici negatif veya 0 girerse ? ==> hata
+        // kullanici 1 girerse ? ==> 0
+        // kullanici 2 girerse ? ==> 0, 1
+        // kullanici 2'den fazla deger girerse ==> 0, 1 .........
+
+
+
+
+    }
+
+
+    // Verilen pozitif bir n tamsayisini alarak,
+    // bize ilk n tane Fibonacci sayisini bir list olarak donduren bir method olusturun.
+
+
+
+}
