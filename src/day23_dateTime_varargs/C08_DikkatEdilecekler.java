e52243cf6df51dd0f6b70922deab02ad5da4e960
bulutluoz <abbulutluoz@gmail.com>
2024-10-23 12:55:36 +0200

    23.gun sonu

--- src/day23_dateTime_varargs/C08_DikkatEdilecekler.java
+++ src/day23_dateTime_varargs/C08_DikkatEdilecekler.java
@@ -8,5 +8,37 @@ public class C08_DikkatEdilecekler {
         // sonuncusu haric geriye kalanlari toplayip
         // bulunan toplam deger ile son sayinin carpimini yazdiran
         // bir method olusturun
+
+        islemYap(2,3,4); // 20
+        islemYap(1,2,3,4,5); // 50
+        islemYap(1,2,3,4,5,6,0); // 0
+
     }
+
+    //public static void islemYap(int... v , String sonSayi){
+        // Vararg parameter must be the last in the list
+        // vararg bir parametre varsa, SON PARAMETRE olmak ZORUNDADIR
+        // vararg element almada sinir tanimaz, tum elementleri almak ister
+
+    //}
+
+    public static void islemYap(int... v){
+
+        // vararg array altyapisini kullandigi icin
+        // son elementi vararg icinde ayirabiliriz
+
+
+        // vararg'da son element haric, bastaki elementleri toplayin
+
+        int toplam = 0;
+
+        for (int i = 0; i <= v.length-2 ; i++) {
+
+            toplam += v[i];
+
+        }
+
+        System.out.println(toplam * v[v.length-1]);
+    }
+
 }
