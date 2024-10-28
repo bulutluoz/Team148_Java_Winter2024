3e048482b9c70584c79ba4cf80673f106583e02b
bulutluoz <abbulutluoz@gmail.com>
2024-10-28 12:20:29 +0100

    25.gun sonu

---
+++ src/day25_encapsulation_inheritance/C07_Inheritance_KissadanHisseler.java
@@ -0,0 +1,39 @@
+package day25_encapsulation_inheritance;
+
+public class C07_Inheritance_KissadanHisseler {
+
+    /*
+        1. slayt
+
+            - child class'lar baslangic itibariyle, parent class'daki tum ozelliklere sahiptir
+              eger isterse parent class'daki ozellikleri update edebilir
+              eger isterse parent class'da olmayan yeni ozellikler ekleyebilir
+
+            - child class'daki ozellikler daha guncel ve daha cok
+              parent class'daki ozellikler ise daha az ve daha genel olur
+
+        2.slayt
+
+            Tek bir obje, tanimlandigi yere gore
+            farkli isimlendirilebilir ve
+            farkli gorev, yetki ve sorumluluklara sahip olabilir
+
+        3.slayt
+
+            - bir obje kendi klasina ait oldugu kadar,
+              parent class'larina da sahiptir
+              ornegin bir servis hemsiresi, ayni zamanda hemsire class'ina,
+                      calisan class'ina ve personel class'inin ozelliklerine de sahiptir
+              Eger bir ust kimlige gecis yaparsa (ornegin personel toplantisina katilan doktor)
+              child class'daki kimligini birakip, ust kimlikteki tum objeleri temsil etmelidir
+
+            - bazi parent class'lar (ornek balik class'i veya tatli su baliklari class'i)
+              direkt objeye sahip degildir
+              Bu class'larin varlik amaci alt class'larda olan
+              ortak ozellikleri bir arada tutmaktir.
+
+
+
+
+     */
+}
