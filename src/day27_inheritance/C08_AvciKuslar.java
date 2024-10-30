746a7e0da677f2b2e867ee144c6c94d23aa61e89
bulutluoz <abbulutluoz@gmail.com>
2024-10-30 12:58:59 +0100

    27.gun sonu

--- src/day27_inheritance/C08_AvciKuslar.java
+++ src/day27_inheritance/C08_AvciKuslar.java
@@ -17,5 +17,91 @@ public class C08_AvciKuslar extends C07_Kuslar{
         System.out.println("sivri gagali");
     }
 
+    public static void main(String[] args) {
+
+        /*
+            EGER ozellikler method olarak olusturuldu ise
+            yine aramaya Data Turu olan Class'dan baslariz
+
+            AMMMAAA bulduktan sonra,
+            Constructor'in oldugu Class'a kadar
+            DAHA GUNCEL BILGI var mi diye kontrol ederiz
+
+
+            data class'indan aramaya basladiginda
+            ozelligi bulamazsa CTE verir
+            guncele de bakmaz
+         */
+
+        // Hayvanlar Class'indan bir obje olusturun
+        C06_Hayvanlar hayvan1 = new C06_Hayvanlar();
+        hayvan1.omur(); // H yasar ve olurler
+        hayvan1.solunum(); // H nefes alir
+        hayvan1.cogalma(); // H cogalirlar
+        // hayvan1.kanat(); // Hayvanlar'da yok CTE
+        hayvan1.hareket(); // H hareket ederler
+        hayvan1.beslenme(); // H Beslenirler
+        //hayvan1.gaga(); // Hayvanlar'da yok CTE
+        // hayvan1.pence();  // Hayvanlar'da yok CTE
+
+
+        // Kuslar Class'indan bir obje olusturun
+
+        C06_Hayvanlar kusHayvani = new C07_Kuslar();
+        kusHayvani.omur(); // H yasar ve olurler
+        kusHayvani.solunum(); // K akcigerle nefes alirlar
+        kusHayvani.cogalma(); // K yumurtayla cogalirlar
+        // kusHayvani.kanat(); // Hayvanlar'da yok CTE
+        kusHayvani.hareket(); // H hareket ederler
+        kusHayvani.beslenme(); // H Beslenirler
+        // kusHayvani.gaga(); // Hayvanlar'da yok CTE
+        // kusHayvani.pence();  // Hayvanlar'da yok CTE
+
+        C07_Kuslar kus1 = new C07_Kuslar();
+        kus1.omur(); // H yasar ve olurler
+        kus1.solunum(); // K akcigerle nefes alirlar
+        kus1.cogalma(); // K yumurtayla cogalirlar
+        kus1.kanat(); // K kanatlidir
+        kus1.hareket(); // H hareket ederler
+        kus1.beslenme(); // H Beslenirler
+        kus1.gaga(); // K gagalari vardir
+        // kus1.pence();  // Kuslar ve Hayvanlar'da yok CTE
+
+        // Avci kuslar class'indan bir obje olusturun
+        C06_Hayvanlar hayvanKartal = new C08_AvciKuslar();
+        hayvanKartal.omur(); // H yasar ve olurler
+        hayvanKartal.solunum(); // K akcigerle nefes alirlar
+        hayvanKartal.cogalma(); // K yumurtayla cogalirlar
+        //hayvanKartal.kanat(); // Hayvanlar'da yok CTE
+        hayvanKartal.hareket(); // A ucarlar
+        hayvanKartal.beslenme(); // A Etle yerler
+        //hayvanKartal.gaga(); // Hayvanlar'da yok CTE
+        //hayvanKartal.pence();  // Hayvanlar'da yok CTE
+
+
+        C07_Kuslar kusKartal = new C08_AvciKuslar();
+        kusKartal.omur(); // H yasar ve olurler
+        kusKartal.solunum(); // K akcigerle nefes alirlar
+        kusKartal.cogalma(); // K yumurtayla cogalirlar
+        kusKartal.kanat(); // K kanatlidir
+        kusKartal.hareket(); // A ucarlar
+        kusKartal.beslenme(); // A Etle yerler
+        kusKartal.gaga(); // A sivri gagalidir
+        // kusKartal.pence();  // Kuslar ve Hayvanlar'da yok CTE
+
+
+        C08_AvciKuslar avciKartal = new C08_AvciKuslar();
+        avciKartal.omur(); // H yasar ve olurler
+        avciKartal.solunum(); // K akcigerle nefes alirlar
+        avciKartal.cogalma(); // K yumurtayla cogalirlar
+        avciKartal.kanat(); // K kanatlidir
+        avciKartal.hareket(); // A ucarlar
+        avciKartal.beslenme(); // A Etle yerler
+        avciKartal.gaga(); // A sivri gagalidir
+        avciKartal.pence();  // A pencelidir
+
+
+    }
+
 
 }
