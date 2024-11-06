31070cd47565da7bef22ae6e631db26a753ee497
bulutluoz <abbulutluoz@gmail.com>
2024-11-06 10:43:19 +0100

    day31 sonu

---
+++ src/day31_exceptions/C06_SayiToplama.java
@@ -0,0 +1,26 @@
+package day31_exceptions;
+
+import java.util.Scanner;
+
+public class C06_SayiToplama {
+
+    public static void main(String[] args) {
+
+        // Kullanicidan toplanmak uzere tamsayilar isteyin
+        // ve kullanici q veya Q'ya basincaya kadar islemi devam ettirin
+        // Kullanici q'ya bastiginda
+        // kac adet sayi girildigini ve toplamin ne oldugunu yazdirin
+
+
+        Scanner scanner = new Scanner(System.in);
+        int toplam = 0;
+        int sayac = 0;
+        int girilenSayi = 0;
+        String girilenMetin = "";
+
+
+        do{
+
+        }while ();
+    }
+}
